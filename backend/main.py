ca5665fcde9718c30b2017a36a34651918577a81
Karthika1403 <karthikaprabha2020@gmail.com>
2026-07-02 13:01:32 +0530

    fix dependencies + cors

--- backend/main.py
+++ backend/main.py
@@ -13,11 +13,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://reops-ai.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_origins=["*"],
 )
 
 app.include_router(router)
